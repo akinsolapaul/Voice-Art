2b117251878e4b05b7f6bfff15929cb1f92092e1
akinsolapaul <40691821+akinsolapaul@users.noreply.github.com>
2020-07-15 22:43:49 +0100

    Restructure

--- how.php
+++ how.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit']))
 {
 	$name=$_POST['name'];
 	$email=$_POST['email'];
-	$conn=new mysqli("localhost","root","","mysite");
+	$conn=new mysqli("x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","xco79bayaya6123v","or42rxd019m8nruh","cdyq2s9qbjxwba6j");
 	$cmd="INSERT into collect value('$name','$email');";
 	if (mysqli_query($conn, $cmd))
 	{
@@ -17,4 +17,4 @@ if (isset($_POST['submit']))
 	}
 }
 
-?>
+?> 
