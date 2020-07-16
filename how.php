40059504c66c5bbe4aa09261a0f6a7cfa8575372
akinsolapaul <40691821+akinsolapaul@users.noreply.github.com>
2020-07-16 11:35:38 +0100

    edited code

--- how.php
+++ how.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit']))
 {
 	$name=$_POST['name'];
 	$email=$_POST['email'];
-	$conn=new mysqli("mysql://xco79bayaya6123v:or42rxd019m8nruh@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cdyq2s9qbjxwba6j","xco79bayaya6123v","or42rxd019m8nruh","cdyq2s9qbjxwba6j");
+	$conn=new mysqli("mysql://b8c6b3fe091a3f:ed48b709@us-cdbr-east-02.cleardb.com/heroku_56fa774bbd1fe14?reconnect=true","b8c6b3fe091a3f","ed48b709","heroku_56fa774bbd1fe14");
 	$cmd="INSERT into collect value('$name','$email');";
 	if (mysqli_query($conn, $cmd))
 	{
